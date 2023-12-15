5418c4f46e13c4b2fe7723445a9b03484df43c42
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-12-15 14:34:27 +0300

    final p2, update readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### freeCodeCamp Back End Development and APIs Projects
 
 > P1 Demo: https://boilerplate-project-timestamp.emreguendogdu.repl.co/
+> P2 Demo: https://boilerplate-project-headerparser.emreguendogdu.repl.co/
--- request-header-parser-microservice/index.js
+++ request-header-parser-microservice/index.js
@@ -2,39 +2,39 @@
 // where your node app starts
 
 // init project
-require('dotenv').config();
-var express = require('express');
-var app = express();
+require("dotenv").config()
+var express = require("express")
+var app = express()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+var cors = require("cors")
+app.use(cors({ optionsSuccessStatus: 200 })) // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"))
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html")
+})
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" })
+})
 
 // work (we're not using :header? , on /api/ ? gets empty value as undefined. not defining gets "Not Found" & :header defines given value as header variable)
-app.get('/api/whoami', (req, res) => {
+// req.header() instead of req.headers[] to prevent confusion because it's not case sensitive
+app.get("/api/whoami", (req, res) => {
   res.send({
-    software: req.headers["user-agent"],
-    language: req.headers["accept-language"],
-    ipaddress: req.headers["x-forwarded-for"]
+    software: req.header("user-agent"),
+    language: req.header("accept-language"),
+    ipaddress: req.header("x-forwarded-for"),
   })
 })
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
-
+  console.log("Your app is listening on port " + listener.address().port)
+})
